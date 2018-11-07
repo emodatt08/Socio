e0b9ce5b20a705540bc879c72505e1aca44e2145
emodatt08 <emodatt08@gmail.com>
2018-11-07 12:21:29 +0000

    added helpers

---
+++ app/Helpers/GoogleMapsHelper.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emodatt08
+ * Date: 07/11/2018
+ * Time: 12:20 PM
+ */
+
+namespace App\Helpers;
+
+
+class GoogleMapsHelper
+{
+
+}
---
+++ app/Helpers/IpApiHelper.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emodatt08
+ * Date: 07/11/2018
+ * Time: 12:19 PM
+ */
+
+namespace App\Helpers;
+
+
+class IpApiHelper
+{
+
+}
